2be8d5429553a08827cd0bbf2de45d97959272d4
spiderwebby <spiderwebby@gmail.com>
2024-04-10 09:01:03 +0100

    working on motor control

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 #include <PPMReader.h>
 
-// Initialize a PPMReader on digital pin 3 with 6 expected channels.
+//! /////////////////////////////////////// !//
+//!         KEEP STUFF MODULAR!             !//
+//! there's gonna be an RPi stuck between   !//
+//!     inputs and outputs eventually!      !//
+//! /////////////////////////////////////// !//
+
+// Initialize a PPMReader on digital pin 3 with 8 expected channels.
 byte interruptPin = 3;
 byte channelAmount = 8;
 PPMReader ppm(interruptPin, channelAmount);
+
+const int LEFT_TRACK_FORWARDS_PIN = 4;
+const int LEFT_TRACK_BACKWARDS_PIN = 5;
+const int RIGHT_TRACK_FORWARDS_PIN = 6;
+const int RIGHT_TRACK_BACKWARDS_PIN = 7;
+const int LEFT_TRACK_ENABLE_PIN = 9;
+const int RIGHT_TRACK_ENABLE_PIN = 10;
+
+int speedDeadzone = 10; //0-255
+int steerDeadzone = 10; //0-255
+
+
+
+void drive(int, int);
 
 void setup() {
     Serial.begin(115200);
@@ -28,4 +48,27 @@ void loop() {
 }
 
 
+void drive(int speed, int steer)
+{
+    //expects -255 to +255 inputs. forward +, right +
+    int leftTrack = 0;
+    int rightTrack = 0;
+    if((speed<speedDeadzone)&&(speed>-speedDeadzone)) speed = 0;
+    if((steer<steerDeadzone)&&(steer>-steerDeadzone)) steer = 0;
+    
+    //steering mixer
+    leftTrack = (speed - steer)/2;
+    rightTrack = (speed + steer)/2;
 
+    if(leftTrack == 0) {
+        analogWrite(LEFT_TRACK_ENABLE_PIN, 0);
+    } else {
+
+    }
+
+    if(rightTrack == 0) {
+        analogWrite(RIGHT_TRACK_ENABLE_PIN, 0);
+    } else {
+
+    }
+}
