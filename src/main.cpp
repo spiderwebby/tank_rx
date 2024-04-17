2540811f067de81c5f4c7aea75d72a297d21fb37
spiderwebby <spiderwebby@gmail.com>
2024-04-17 10:25:06 +0100

    set pinmodes *sigh*

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ const int RIGHT_TRACK_BACKWARDS_PIN = 5;
 const int LEFT_TRACK_ENABLE_PIN = 9;
 const int RIGHT_TRACK_ENABLE_PIN = 10;
 
-int rx[channelAmount]={0,0,0,0,-255,0,0,0};
+int rx[channelAmount]={0,0,0,0,0,0,0,0};
 //0-3 are the sticks
 //4 is the button
 //5-7 are knobs
 
-int speedDeadzone = 10; //0-255
-int steerDeadzone = 10; //0-255
+int speedDeadzone = 25; //0-255
+int steerDeadzone = 25; //0-255
 
 
 
@@ -33,6 +33,12 @@ void drive(int, int);
 
 void setup() {
     Serial.begin(115200);
+    pinMode(LEFT_TRACK_FORWARDS_PIN, OUTPUT);
+    pinMode(LEFT_TRACK_BACKWARDS_PIN, OUTPUT);
+    pinMode(RIGHT_TRACK_FORWARDS_PIN, OUTPUT);
+    pinMode(RIGHT_TRACK_BACKWARDS_PIN, OUTPUT);
+    pinMode(LEFT_TRACK_ENABLE_PIN, OUTPUT);
+    pinMode(RIGHT_TRACK_ENABLE_PIN, OUTPUT);
 }
 
 
@@ -40,7 +46,8 @@ void loop() {
     // Print latest valid values from all channels
     for (byte channel = 0; channel <= (channelAmount-1); ++channel) {
         unsigned value = ppm.latestValidChannelValue(channel+1, 0);
-        rx[channel]=map(value,1876,1060,255,-255);
+        if(channel==5) rx[channel]=map(value,1876,1060,-255,255); //5 needs to be inverted
+        else rx[channel]=map(value,1876,1060,255,-255);
         Serial.print(">rx"); 
         Serial.print(channel); 
         Serial.print(" : "); 
@@ -53,12 +60,21 @@ void loop() {
 
 
     drive(rx[1], rx[0]);
-    Serial.print(">drive1:");
-    Serial.println(rx[1]);
-    
-    Serial.print(">drive0:");
-    Serial.println(rx[0]);
-    Serial.println(">button:pressed|t");
+
+/*
+    if(rx[4]>0) {
+        Serial.println(">button:pressed|t");
+        digitalWrite(LEFT_TRACK_FORWARDS_PIN, 1);
+        digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
+        digitalWrite(LEFT_TRACK_ENABLE_PIN,1);
+    }
+    else {
+        Serial.println(">button:released|t");
+        digitalWrite(LEFT_TRACK_FORWARDS_PIN, 0);
+        digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
+        digitalWrite(LEFT_TRACK_ENABLE_PIN,0);
+    }
+*/   
 
 
     delay(20);
@@ -74,15 +90,25 @@ void drive(int speed, int steer)
     int rightTrack = 0;
     if((speed<speedDeadzone)&&(speed>-speedDeadzone)) speed = 0;
     if((steer<steerDeadzone)&&(steer>-steerDeadzone)) steer = 0;
+
+    
     
     //steering mixer
-    leftTrack = (-speed - steer)/2;
+    leftTrack = (-speed - steer);
     leftTrack = (leftTrack < 255) ? leftTrack : 255; //cap max values
+    leftTrack = (leftTrack > -255) ? leftTrack : -255; //cap max values
     leftTrack = (leftTrack > 240) ? 255: leftTrack;  //top deadzone
     
-    rightTrack = (-speed + steer)/2;
+    rightTrack = (-speed + steer);
     rightTrack = (rightTrack < 255) ? rightTrack : 255;
+    rightTrack = (rightTrack > -255) ? rightTrack : -255;
     rightTrack = (rightTrack > 240) ? 255: rightTrack;
+
+    Serial.print(">leftTrack:");
+    Serial.println(leftTrack);
+    
+    Serial.print(">rightTrack:");
+    Serial.println(rightTrack);
 
     if(leftTrack == 0) {
         analogWrite(LEFT_TRACK_ENABLE_PIN, 0);
