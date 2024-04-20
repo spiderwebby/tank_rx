ca72969fca6601bf3ccedfc48e0af7590f48254a
spiderwebby <spiderwebby@gmail.com>
2024-04-20 01:23:58 +0100

    added mpu6050

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <PPMReader.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include<Wire.h>
 
 //! /////////////////////////////////////// !//
 //!         KEEP STUFF MODULAR!             !//
@@ -11,6 +14,8 @@
 byte interruptPin = 7; //pin7
 const byte channelAmount = 8;
 PPMReader ppm(interruptPin, channelAmount);
+
+const int MPU=0x68; 
 
 const int LEFT_TRACK_FORWARDS_PIN = 6;
 const int LEFT_TRACK_BACKWARDS_PIN = 8;
@@ -24,12 +29,18 @@ int rx[channelAmount]={0,0,0,0,0,0,0,0};
 //4 is the button
 //5-7 are knobs
 
-int speedDeadzone = 25; //0-255
-int steerDeadzone = 25; //0-255
+const int speedDeadzone = 25; //0-255 //10 is the noisefloor
+const int steerDeadzone = 25; //0-255
+const int trackDeadzone = 1;
+int trackPreload = 0;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 
+Adafruit_MPU6050 mpu;
+sensors_event_t a, g, temp;
 
 void drive(int, int);
+void readMPU();
 
 void setup() {
     Serial.begin(115200);
@@ -39,6 +50,12 @@ void setup() {
     pinMode(RIGHT_TRACK_BACKWARDS_PIN, OUTPUT);
     pinMode(LEFT_TRACK_ENABLE_PIN, OUTPUT);
     pinMode(RIGHT_TRACK_ENABLE_PIN, OUTPUT);
+    mpu.begin();
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+    delay(100);
 }
 
 
@@ -48,38 +65,40 @@ void loop() {
         unsigned value = ppm.latestValidChannelValue(channel+1, 0);
         if(channel==5) rx[channel]=map(value,1876,1060,-255,255); //5 needs to be inverted
         else rx[channel]=map(value,1876,1060,255,-255);
+        if(rx[channel]>255) rx[channel]=255;
+        else if(rx[channel]<-255) rx[channel]=-255;
         Serial.print(">rx"); 
         Serial.print(channel); 
         Serial.print(" : "); 
         Serial.print(rx[channel]); 
         //Serial.print("§deg");
         Serial.print("\n");
-        //if(channel < channelAmount) Serial.print('\t');
     }
-    //Serial.println();
 
 
     drive(rx[1], rx[0]);
 
-/*
-    if(rx[4]>0) {
-        Serial.println(">button:pressed|t");
-        digitalWrite(LEFT_TRACK_FORWARDS_PIN, 1);
-        digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
-        digitalWrite(LEFT_TRACK_ENABLE_PIN,1);
-    }
-    else {
-        Serial.println(">button:released|t");
-        digitalWrite(LEFT_TRACK_FORWARDS_PIN, 0);
-        digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
-        digitalWrite(LEFT_TRACK_ENABLE_PIN,0);
-    }
-*/   
 
+    
+    readMPU();
 
     delay(20);
-    
+}
 
+void readMPU() {
+
+    mpu.getEvent(&a, &g, &temp);
+
+    
+    Serial.print(">AcX:"); Serial.println(a.acceleration.x); 
+    Serial.print(">AcY:"); Serial.println(a.acceleration.y); 
+    Serial.print(">AcZ:"); Serial.println(a.acceleration.z); 
+    
+    Serial.print(">GyX:"); Serial.println(g.gyro.x); 
+    Serial.print(">GyY:"); Serial.println(g.gyro.y); 
+    Serial.print(">GyZ:"); Serial.println(g.gyro.z); 
+    
+    Serial.print(">temp:"); Serial.println(temp.temperature); 
 }
 
 
@@ -88,58 +107,105 @@ void drive(int speed, int steer)
     //expects -255 to +255 inputs. forward +, right +
     int leftTrack = 0;
     int rightTrack = 0;
-    if((speed<speedDeadzone)&&(speed>-speedDeadzone)) speed = 0;
-    if((steer<steerDeadzone)&&(steer>-steerDeadzone)) steer = 0;
+    trackPreload = (rx[5]+257)/4;
 
+    if((speed<speedDeadzone)&&(speed>-speedDeadzone)) speed = 0;
+    else if(speed<speedDeadzone) speed = speed+speedDeadzone;
+         else speed = speed-speedDeadzone;
+    if((steer<steerDeadzone)&&(steer>-steerDeadzone)) steer = 0;
+    else if(steer<speedDeadzone) steer = steer+speedDeadzone;
+         else steer = steer-speedDeadzone;
+
+    Serial.print(">speed:");
+    Serial.println(speed);
     
-    
+    Serial.print(">steer:");
+    Serial.println(steer);
+
     //steering mixer
-    leftTrack = (-speed - steer);
+    leftTrack  = speed + steer;
+    rightTrack = speed - steer;
+
+    if(speed > 0 && leftTrack > speed)
+        leftTrack = speed;
+    if(speed > 0 && rightTrack > speed)
+        rightTrack = speed;
+    if(speed < 0 && leftTrack < speed)
+        leftTrack = speed;
+    if(speed < 0 && rightTrack < speed)
+        rightTrack = speed;
+    
+
+
+    //trackspeed response curve
+    if(leftTrack>0)
+        leftTrack = 2+(2*(pow(2,(float(leftTrack)/32.0))));   //change linear stick movement to curve response
+    else
+        leftTrack = 2-(2*(pow(2,(float(abs(leftTrack)/32.0))))); //because the mafs don't work for negative values
+    
+    if(rightTrack>0)
+        rightTrack = 2+(2*(pow(2,(float(rightTrack)/32.0))));   //change linear stick movement to curve response
+    else
+        rightTrack = 2-(2*(pow(2,(float(abs(rightTrack)/32.0))))); //because the mafs don't work for negative values
+
+
+    //cap max track speed
     leftTrack = (leftTrack < 255) ? leftTrack : 255; //cap max values
     leftTrack = (leftTrack > -255) ? leftTrack : -255; //cap max values
-    leftTrack = (leftTrack > 240) ? 255: leftTrack;  //top deadzone
+    //leftTrack = (leftTrack > 240) ? 255: leftTrack;  //top deadzone
     
-    rightTrack = (-speed + steer);
     rightTrack = (rightTrack < 255) ? rightTrack : 255;
     rightTrack = (rightTrack > -255) ? rightTrack : -255;
-    rightTrack = (rightTrack > 240) ? 255: rightTrack;
+    //rightTrack = (rightTrack > 240) ? 255: rightTrack;
 
-    Serial.print(">leftTrack:");
-    Serial.println(leftTrack);
     
-    Serial.print(">rightTrack:");
-    Serial.println(rightTrack);
+    Serial.print(">trackDeadZone:");
+    Serial.println(trackDeadzone);
+    Serial.print(">negtrackDeadZone:");
+    Serial.println(-trackDeadzone);
 
-    if(leftTrack == 0) {
+    if((leftTrack < trackDeadzone) && (leftTrack > -trackDeadzone)) {
+        leftTrack = 0;
         analogWrite(LEFT_TRACK_ENABLE_PIN, 0);
         digitalWrite(LEFT_TRACK_FORWARDS_PIN, 0);
         digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
     } else {
-        if(leftTrack > 0) {
+        if(leftTrack > -trackDeadzone) {
             digitalWrite(LEFT_TRACK_FORWARDS_PIN, 1);
             digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 0);
-        } else if(leftTrack < 0) {
+        } else if(leftTrack < trackDeadzone) {
             digitalWrite(LEFT_TRACK_FORWARDS_PIN, 0);
             digitalWrite(LEFT_TRACK_BACKWARDS_PIN, 1);
         }
-        //analogWrite(LEFT_TRACK_ENABLE_PIN, abs(leftTrack));
-        analogWrite(LEFT_TRACK_ENABLE_PIN, map(abs(leftTrack),0,255,rx[4],255));
+        leftTrack = map(abs(leftTrack),0,255,trackPreload,255);
+        analogWrite(LEFT_TRACK_ENABLE_PIN, abs(leftTrack));
     }
 
-    if(rightTrack == 0) {
+    if(rightTrack < trackDeadzone && rightTrack > -trackDeadzone) {
+        rightTrack = 0;
         analogWrite(RIGHT_TRACK_ENABLE_PIN, 0);
         digitalWrite(RIGHT_TRACK_FORWARDS_PIN, 0);
         digitalWrite(RIGHT_TRACK_BACKWARDS_PIN, 0);
     } else {
-        if(rightTrack > 0) {
+        if(rightTrack > -trackDeadzone) {
             digitalWrite(RIGHT_TRACK_FORWARDS_PIN, 1);
             digitalWrite(RIGHT_TRACK_BACKWARDS_PIN, 0);
-        } else if(rightTrack < 0) {
+        } else if(rightTrack < trackDeadzone) {
             digitalWrite(RIGHT_TRACK_FORWARDS_PIN, 0);
             digitalWrite(RIGHT_TRACK_BACKWARDS_PIN, 1);
         }
+        rightTrack = map(abs(rightTrack),0,255,trackPreload,255);
         analogWrite(RIGHT_TRACK_ENABLE_PIN, abs(rightTrack));
     }
+
+
+
+    
+
+    Serial.print(">leftTrack:");
+    Serial.println(leftTrack);
+    Serial.print(">rightTrack:");
+    Serial.println(rightTrack);
 /*
     if(rightTrack == 0) {
         analogWrite(RIGHT_TRACK_ENABLE_PIN, 0);
